21c1cfede12080e69a0173dd8fe0ce14d1f888be
Ghous Khan <ghousk@hotmail.com>
2025-04-16 19:19:02 +0100

    Updated ViewModel Tests

--- app/src/test/java/com/iq/newbaseappmarch25/GithubViewModelTest.kt
+++ app/src/test/java/com/iq/newbaseappmarch25/GithubViewModelTest.kt
@@ -1,17 +1,17 @@
 package com.iq.newbaseappmarch25
 
+import com.iq.newbaseappmarch25.api.GithubApiService
 import com.iq.newbaseappmarch25.model.User
 import com.iq.newbaseappmarch25.repository.GithubRepository
 import com.iq.newbaseappmarch25.viewmodel.GithubViewModel
 import io.mockk.coEvery
 import io.mockk.mockk
-import kotlinx.coroutines.runBlocking
-import kotlin.test.assertEquals
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.test.runTest
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
-import kotlin.test.assertTrue
+import kotlin.test.assertEquals
 
 /**********************************************************************
  * Copyright 2025 Innovative Quest Ltd
@@ -25,36 +25,41 @@ import kotlin.test.assertTrue
 class GithubViewModelTest {
 
     private lateinit var viewModel: GithubViewModel
-    private val mockRepository = mockk<GithubRepository>()
+    private lateinit var repository: GithubRepository
+    private val mockApiService = mockk<GithubApiService>()
 
     @Before
     fun setup() {
-        viewModel = GithubViewModel(mockRepository)
+        repository = GithubRepository(mockApiService)
+        viewModel = GithubViewModel(repository)
     }
 
-//    @Test
-//    fun `fetchUsers should update users list`() = runBlocking {
-////        val mockUsers = listOf(User(1, "user1", "url1", "name1", "bio1", 10, 5, 2))
-////        coEvery { mockRepository.getUsers() } returns mockUsers
-//
-//        viewModel.fetchUsers()
-//
-//        val users = viewModel.users
-//
-//        assertEquals(1, users.size)
-//    }
+    @Test
+    fun `fetchUsers should update users list`() = runTest {
+        val mockUsers = listOf(User(1, "user1", "url1", "name1", "bio1", 10, 5, 2))
+        coEvery { mockApiService.getUsers() } returns mockUsers
 
-//    @Test
-//    fun testFetchItems() = runBlocking {
-//        viewModel.fetchUsers()
-//        val items = viewModel.users
-//        assertTrue (items.size > 5)
-//    }
+        backgroundScope.launch {
+            val job = viewModel.fetchUsers()
+            job.invokeOnCompletion {
+                val users = viewModel.users
+                assertEquals(1, users.size)
+            }
+        }
+    }
 
-//    @Test
-//    fun testFetchItemById() = runTest {
-//        viewModel.fetchItemById("1")
-//        val item = viewModel.selectedItem.first()
-//        assertEquals("Item 1", item?.name)
-//    }
+    @Test
+    fun `fetchItemById should fetch a user by the given id`() = runTest {
+        val mockUser = User(1, "user1", "url1", "name1", "bio1", 10, 5, 2)
+        coEvery { mockApiService.getUserDetails("user1") } returns mockUser
+
+        backgroundScope.launch {
+            val job = viewModel.fetchUserDetails("user1")
+            job.invokeOnCompletion {
+                val user = viewModel.selectedUser
+                assertEquals("name1", user?.name)
+            }
+        }
+    }
+
 }
